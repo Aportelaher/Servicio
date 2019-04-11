dc5f75554780ba5a511f3595bff2effbf2147a9f
Aportelaher <hugo.aportela.20@gmail.com>
2019-04-10 21:14:56 -0500

    l

--- validaExp.php
+++ validaExp.php
@@ -1,17 +1,26 @@
 <?php
-      if (isset($_POST['expresion'])=="uno") 
-      { 
+      $expresion = $_POST['expresion'];
+       var_dump($_POST);
+
+       if (isset($expresion)) 
+       {
+         
+         if ($_POST['expresion']=="uno") 
+        { 
           header("location:Saludo.php");
-      }
-      else
-      if (isset($_POST['expresion'])=="2") 
-      { 
+        }
+        else
+        if ($_POST['expresion']=="dos") 
+        { 
           header("location:Dicho.php");
-      }
-      else
-      if (isset($_POST['expresion'])=="tres") 
-      { 
+        }
+        else
+        if ($_POST['expresion']=="tres") 
+        { 
           header("location:Modismo.php");
-      }
+        }
+          
+       }
+      
 
 ?>
