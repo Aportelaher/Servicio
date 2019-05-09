f3e6c3f8c33ac73057d28482aaaa1d535ac661a7
IgnacioOrt <comosea1122@hotmail.com>
2019-05-08 19:34:38 -0500

    Palabra en una sola columna

--- Palabra.php
+++ Palabra.php
@@ -117,18 +117,18 @@
                 {
                   while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
                   {
-                    ?>
-                    <div class="row mt-3">
-                      <div class="col-md-6 col-12">
-                        <div class="card">
-                          <div class="card-header text-center">
-                            <?php
-                              $context = $row['Campo_Semántico'];
-                            ?>
-                            <a href="Contexto.php?contexto=<?php echo ($context); ?>&palabra=<?php echo ($palabra); ?>"><?php echo ($context); ?></a>
-                          </div>
-                          <div class="card-body">
-                            <?php
+                    $context = $row['Campo_Semántico'];
+                    if ($context != '') {
+                      ?>
+                      <div class="row mt-3">
+                        <div class="col-md-12 col-12">
+                          <div class="card">
+                            <div class="card-header text-center">
+
+                              <a href="Contexto.php?contexto=<?php echo ($context); ?>&palabra=<?php echo ($palabra); ?>"><?php echo ($context); ?></a>
+                            </div>
+                            <div class="card-body">
+                              <?php
                               $query2 = "SELECT Palabra, Traducción FROM concentrado_P WHERE Campo_Semántico = '" . $row['Campo_Semántico'] ."' && Palabra LIKE '%$palabra%' LIMIT 0,2";
                               $result2=mysqli_query($link,$query2);
                               ?>
@@ -158,56 +158,12 @@
                                   ?>
                                 </tbody>
                               </table> 
+                            </div>
                           </div>
                         </div>
                       </div>
                       <?php
-                      if ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-                        ?>
-                          <div class="col-md-6 col-12 col-sm-12">
-                            <div class="card">
-                              <div class="card-header text-center">
-                                <a href="Contexto.php?contexto=<?php echo ($context); ?>&palabra=<?php echo ($palabra); ?>"><?php echo ($context); ?></a>
-                              </div>
-                              <div class="card-body">
-                                <?php
-                                $query2 = "SELECT Palabra, Traducción FROM concentrado_P WHERE Campo_Semántico = '" . $row['Campo_Semántico'] ."' && Palabra LIKE '%$palabra%' LIMIT 0,2";
-                                $result2=mysqli_query($link,$query2);
-                                ?>
-                                <table class="table">
-                                  <thead>
-                                    <tr>
-                                      <th>Palabra</th>
-                                      <th>Traducción</th>
-                                    </tr>
-                                  </thead>
-                                  <tbody>
-                                    <?php
-                                    if (mysqli_num_rows($result2)>0)
-                                    {
-                                      while ($row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC))
-                                      {
-                                        $palab = $row2["Palabra"];
-                                        $trad = $row2["Traducción"];
-                                        ?>
-                                        <tr>
-                                          <td><?php echo "$palab"; ?></td>
-                                          <td><?php echo "$trad"; ?></td>
-                                        </tr>
-                                        <?php
-                                      }
-                                    }
-                                    ?>
-                                  </tbody>
-                                </table> 
-                              </div>
-                            </div>
-                          </div>
-                        <?php                      
-                      }
-                      ?>
-                    </div>
-                    <?php
+                    }
                   }
                 }else{
                   echo "Fallo al obtener Contextos";
